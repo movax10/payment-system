fbecdcfe3740e07a18c764523fe02442567ff925
Uliana Kucheriava <movax10@gmail.com>
2015-08-03 15:37:59 +0300

    openshift deploying

--- src/main/java/io/github/movax10/controller/AdminController.java
+++ src/main/java/io/github/movax10/controller/AdminController.java
@@ -1,7 +1,9 @@
 package io.github.movax10.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/admin")
 public class AdminController
 {
